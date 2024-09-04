36262252c7209415c87226da99902191f8b6d06c
therizwanparvez <anqparvez@gmail.com>
2024-09-05 04:05:50 +0600

    Create Sign up Functionality

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
 
 import { connectDB } from "../config/db.js";
 
 const app = express();
 dotenv.config();
 connectDB();
+
+app.use(express.json());
+
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
 
 const port = process.env.PORT || 3000;
 
